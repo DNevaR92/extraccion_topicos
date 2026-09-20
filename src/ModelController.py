474e0408bae816349c40e33ba517de7f3cb6b54e
Murloc198 <dc.rubio@uniandes.edu.co>
2026-09-20 19:42:20 +0000

    Final despliegue

--- src/ModelController.py
+++ src/ModelController.py
@@ -1,6 +1,7 @@
 import joblib
 import os
-from preprocess_ut import apply_text_preprocess  # necesario para que joblib.load() resuelva la referencia
+import pandas as pd
+from preprocess_ut import apply_text_preprocess
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MODELS_DIR = os.path.join(BASE_DIR, "resources", "models")
@@ -10,7 +11,8 @@ tsvd = joblib.load(os.path.join(MODELS_DIR, "svd.joblib"))
 modelo_svm = joblib.load(os.path.join(MODELS_DIR, "model.joblib"))
 
 def predict(texto):
-    tfidf_vec = pipeline.transform([texto])
+    texto_series = pd.Series([texto])          # <-- cambio clave
+    tfidf_vec = pipeline.transform(texto_series)
     lsa_vec = tsvd.transform(tfidf_vec)
     ods_predicho = modelo_svm.predict(lsa_vec)
     return ods_predicho[0]
